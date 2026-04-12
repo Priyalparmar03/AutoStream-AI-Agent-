fc0d8d7b358a84833d667086659d8218c2cc5b2b
Priyal parmar <parmarpriyal1603@gmail.com>
2026-04-12 21:04:14 +0530

    Update state.py

--- agent/state.py
+++ agent/state.py
@@ -1,11 +1,3 @@
-# class AgentState:
-#     def __init__(self):
-#         self.name = None
-#         self.email = None
-#         self.platform = None
-#         self.intent = None
-
-
 class AgentState:
     def __init__(self):
         self.user_input = ""
